6b4eedd768dc532855b87443dbdd2a70930c8bba
Stoica <stoicatedy@gmail.com>
2019-05-26 08:29:57 +0300

    trying something new

---
+++ new/assets/js/main.js
@@ -0,0 +1,25 @@
+$("document").ready(function() {
+  $("#info").click(function() {
+    $("#right_panel").attr("src", "./pages/1.html#info");
+  });
+});
+
+setTimeout(function() {
+  $("#leftloading").addClass("animated fadeOut");
+  $("#rightloading").addClass("animated fadeOut");
+  setTimeout(function() {
+    $("#leftloading").removeClass("animated fadeOut");
+    $("#rightloading").removeClass("animated fadeOut");
+    $("#leftloading").css("display", "none");
+    $("#rightloading").css("display", "none");
+    $("#loading").css("display", "none");
+  }, 500);
+}, 1500);
+
+setTimeout(function() {
+  $("#text").addClass("animated fadeOut");
+  setTimeout(function() {
+    $("#text").removeClass("animated fadeOut");
+    $("#text").css("display", "none");
+  }, 500);
+}, 1000);
